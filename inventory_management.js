3d0963e3c2eb797c22ae70378e2a56bd6968a9b5
Quirianyailet Perez <qperez1@usf.edu>
2024-09-13 17:53:38 -0400

    Create displayProductDetails function

--- inventory_management.js
+++ inventory_management.js
@@ -1,9 +1,22 @@
 // Task 1: Create an Inventory Array of Product Objects
-let products = [
-    {name: "laptop",price: `$1000`, quantity: `15`, lowStocklevel: `5`},
-    {name: "mouse",price: `$25`, quantity: `30`, lowStocklevel: `10`},
-    {name: "monitor",price: `$250`, quantity: `25`, lowStocklevel: `4`},
-    {name: "keyboard",price: `$30`, quantity: `30`, lowStocklevel: `10`},
-    {name: "speakers",price: `$350`, quantity: `20`, lowStocklevel: `5`},
-];
-console.log(products);
+
+const inventory = [
+{ name: 'Laptop', price: 1200, quantity: 10, lowStockLevel: 3 },
+{ name: 'Smartphone', price: 800, quantity: 5, lowStockLevel: 2 },
+{ name: 'Tablet', price: 400, quantity: 7, lowStockLevel: 1 },
+{ name: 'Headphones', price: 100, quantity: 15, lowStockLevel: 5 },
+{ name: 'Smartwatch', price: 250, quantity: 3, lowStockLevel: 1 }
+    ];
+console.log(inventory); // Output: An array of 5 products
+
+// Task 2: Create a Function to Display Product Details
+function displayProductDetails(products,stock) {
+products = inventory.name,inventory.price,inventory.quantity
+stock = inventory.lowStockLevel
+if (inventory.quantity >= inventory.lowStockLevel) {
+    return "In Stock"; } 
+   if (inventory.quantity <= inventory.lowStockLevel) {
+    return "Low Stock"; }
+    return products,stock
+;
+}; console.log(displayProductDetails(inventory[0],3))
